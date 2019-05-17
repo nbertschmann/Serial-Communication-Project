e8b1fa483d00c6b5cf3673f0e0b52d8e91d033ec
Noah Bertschmann <bertschmann@ferrovac.com>
2019-05-17 17:06:32 +0200

    communication with arduino set up. script is able to read analog inputs from arduino pins and print them

--- serial_comunication_pyserial.py
+++ serial_comunication_pyserial.py
@@ -1,14 +1,38 @@
 import serial.tools.list_ports
-portData = serial.tools.list_ports.comports()
+
+def findPorts():
+    ports = serial.tools.list_ports.comports()
+    return ports
 
 
-for i, j in enumerate(portData):
-    print(f"{i+1})", end = " ")
-    print(j)
+def findArduino(ports_found):
 
-print(len(portData))
+    com_found = 'None'
+    port_count = len(ports_found)
 
+    for i in range(0,port_count):
+        port = ports_found[i]
+        string_port = str(port)
 
-for i in range(0,len(portData)):
-    print(i);
+        if 'Serielles' or 'Arduinio' in string_port:
+            split_port = string_port.split();
+            com_found = split_port[0]
+
+    return com_found
+
+my_ports = findPorts()
+arduino_port = findArduino(my_ports)
+
+if arduino_port == 'None':
+    print("Error - port not found")
+
+else:
+    print(f"Arduino Port: {arduino_port}")
+
+ser = serial.Serial(arduino_port, baudrate = 9600, timeout = 1)
+
+while 1:
+
+    arduinoData = ser.readline().decode('ascii')
+    print(arduinoData)
 
